a3da7b9d195cd8a45d4dc9d1c793f72425a6e186
jojo jaballas <jojo@dac00001rapp002.com>
2020-09-06 23:13:20 +0000

    first commit

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.6
+#
+# Filename: helloworld.py
+#
+# Name: Jojo Jaballas
+#
+# Description: First python script to display something ont he screen.
+#
+# Date: 09/06/2020
+# Version: 0.0
+
+print("Hello, world!")
