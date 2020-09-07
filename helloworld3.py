6a7dd90cc07669ba731e89ac0f4ae3eb7d416ef8
Jojo Jaballas <jojo@jaballas.com>
2020-09-06 21:50:27 -0400

    Update from a0001v5rapp7222

---
+++ helloworld3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.6
+#
+# Filename: helloworld3.py
+#
+# Name: Jojo Jaballas
+#
+# Description: First python script to display something ont he screen.
+#
+# Date: 09/06/2020
+# Version: 0.2
+
+name=input("Enter your name:")
+print("Hello,",name)
+print("Hello world of coronavirus")
