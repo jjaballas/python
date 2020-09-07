6ad5a34661f899933bedfedde47b18fbf025976b
Jojo Jaballas <jojo@jaballas.com>
2020-09-07 15:03:26 -0400

    0907201202

---
+++ display_card.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3.8
+#
+# Filename: display_card.py
+#
+# Name: Jojo Jaballas
+#
+# Description: Script to display business card using functions.
+#
+# Date: 09/07/2020
+# Version: 0.0
+
+def printvcard():
+    print("Jojo Jaballas")
+    print("Platform Unix/Linux Engineer")
+    print("email: jjaballas@yahoo.com")
+    print("phone: 408-657-9913")
+
+# main call
+if __name__ == '__main__':
+    printvcard()
--- gitsync.sh
+++ gitsync.sh
@@ -4,6 +4,8 @@
 if [ "$#" -ne 2 ]; then
 	echo "Illegal number of parameters"
 	echo "Usage: $0 arg1 arg2"
+	echo "where arg1 is file(s) to add"
+	echo "      arg2 is the comment"
 	exit 2
 fi
 FILETOADD=$1
