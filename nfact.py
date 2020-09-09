0650dbd3fbdc28de4fb8eb8f9a0f0cd5bd19ee52
jojo jaballas <jojo@dac00001rapp002.com>
2020-09-09 21:57:01 +0000

    Updated with use of function

--- nfact.py
+++ nfact.py
@@ -1,22 +1,30 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3.6
 #
 # Filename: nfact.py
+#
+# Usage: nfact.py some_number
 #
 # Name: Jojo Jaballas
 #
 # Description: Python script to calculated the factorial of a number.
 #
 # Date: 09/09/2020
-# Version: 0.1
+# Version: 0.2 Updated with function.
+
 import sys
 
 n=int(sys.argv[1])
-nfact = 1
-j = n
-i = 1
-while i <= j:
-   #print(n,"\t",i,"\t",nfact)
-   nfact = nfact * n
-   n -= 1
-   i += 1
-print(j,"! is ",nfact,sep='')
+
+def nfactorial(n):
+   j = n
+   i = 1
+   nfact = 1
+   while i <= j:
+       nfact = nfact * n
+       n = n - 1
+       i = i + 1
+   return nfact
+
+if __name__ == "__main__":
+   print(n,"! is ",nfactorial(n),sep='')
+
