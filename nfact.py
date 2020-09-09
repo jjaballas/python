b40de99a17c74dd5234f9025ac11ed112f566846
jojo jaballas <jojo@dac00001rapp002.com>
2020-09-09 20:53:41 +0000

    Added N factorial script.

---
+++ nfact.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3.6
+#
+# Filename: nfact.py
+#
+# Name: Jojo Jaballas
+#
+# Description: Python script to calculated the factorial of a number.
+#
+# Date: 09/09/2020
+# Version: 0.1
+n = int(input())
+nfact = 1
+j = n
+i = 1
+while i <= j:
+   print(n,"\t",i,"\t",nfact)
+   nfact = nfact * n
+   n -= 1
+   i += 1
