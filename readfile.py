5fc3fa58025471ae0218dfba97cb6ffade24ae01
Jojo Jaballas <jjaballas@yahoo.com>
2020-10-10 15:08:08 -0700

    added readfile.py

---
+++ readfile.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+# Name: Jojo Jaballas
+# 
+# Description: Program to test reading a file
+# 
+# Date: 10/10/20
+f = open("c:/users/jojo/Desktop/testfile.txt","r")
+print(f.readline())
+f.close()
